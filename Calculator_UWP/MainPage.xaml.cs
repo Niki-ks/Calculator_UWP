e381980ca5b8c5b6a25a59ce8daa5251e152af60
никита мажара <openmail300997@gmail.com>
2021-05-18 15:54:14 -0400

    Добавьте файлы проекта.

---
+++ Calculator_UWP/MainPage.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace Calculator_UWP
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        string input = string.Empty;
+        string x = string.Empty;
+        string y = string.Empty;
+        char operation;
+        double result = 0.0;
+       
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "1";
+            this.textBox.Text += input;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "2";
+            this.textBox.Text += input;
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "3";
+            this.textBox.Text += input;
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "4";
+            this.textBox.Text += input;
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "5";
+            this.textBox.Text += input;
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "6";
+            this.textBox.Text += input;
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "7";
+            this.textBox.Text += input; 
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "8";
+            this.textBox.Text += input;
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            this.textBox.Text = " ";
+            input += "9";
+            this.textBox.Text += input;
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            //VS podskazala chto mozno yprostuti kod ybrav "this" a pisal I "this" kak bulo v primerah na METANITE
+            textBox.Text = " ";
+            input += "0";
+            textBox.Text += input;
+        }
+
+        private void ButtonAC_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "0";
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            x = input;
+            operation = '+'; ;
+            input = string.Empty;
+        }
+
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            x = input;
+            operation = '-';
+            input = string.Empty;
+        }
+
+        private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            x = input;
+            operation = '*';
+            input = string.Empty;
+        }
+
+        private void ButtonDivision_Click(object sender, RoutedEventArgs e)
+        {
+            x = input;
+            operation = '/';
+            input = string.Empty;
+        }
+
+        private void ButtonDouble_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = " ";
+            input = ".";
+            textBox.Text += input;
+
+        }
+
+        private void ButtonEqualy_Click(object sender, RoutedEventArgs e)
+        {
+            y = input;
+            double num1, num2;
+
+            double.TryParse(x, out num1);
+            double.TryParse(y, out num2);
+
+            if (operation == '+')
+            {
+                result = num1 + num2;
+                textBox.Text = result.ToString();
+            }
+            else if (operation == '-')
+            {
+                result = num1 - num2;
+                textBox.Text = result.ToString();
+            }
+            else if (operation == '*')
+            {
+                result = num1 * num2;
+                textBox.Text = result.ToString();
+            }
+            else if (operation == '/')
+                if (num2 != 0)
+            {
+                    result = num1 / num2;
+                    textBox.Text = result.ToString();
+            }
+            else
+                {
+                    textBox.Text = "NE NEHUYA, PROBYU SNOWA";
+                }
+        }
+    }
+}
